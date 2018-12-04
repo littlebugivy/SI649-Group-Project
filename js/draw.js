6e6b2ac74216fe614264e78e39f669fe6fe4dc5d
Ivy Lin <littlebugivy@gmail.com>
2018-12-04 14:12:00 -0500

    style change and bug fix

--- js/draw.js
+++ js/draw.js
@@ -2,12 +2,24 @@
 $(document).ready(function () {
     loadData();
 });
-
+const COLOR_RED = '#EA2327'
+const COLOR_BK = '#151515'
 var numOfMovie;
 var radius = 300;
 var movie_node_size = 48;
-var character_node_size = 25;
+var char_node_size = 25;
 var char_node_size_enlarged = 28;
+var movie_node_width = 110;
+var movie_node_height = 50;
+var active_char;
+var hover_char;
+
+var poly = [{ "x": 0, "y": 10 },
+{ "x": 0, "y": 50 },
+{ "x": 90, "y": 50 },
+{ "x": 110, "y": 40 },
+{ "x": 110, "y": 0 },
+{ "x": 20, "y": 0 }];
 
 var test_data = {
     "nodes": [
@@ -62,7 +74,7 @@ function drawNodes() {
     const width = window.innerWidth
     const height = window.innerHeight
 
-    var svg = d3.select('.container')
+    var svg = d3.select('.graph-container')
         .append('svg')
         .attr('width', width)
         .attr('height', height)
@@ -85,7 +97,7 @@ function drawNodes() {
         if (node.group == 0) {
             return 'white'
         } else {
-            return 'black'
+            return COLOR_BK
         }
     }
 
@@ -99,13 +111,13 @@ function drawNodes() {
     function getNodeSize(node) {
         if (node == undefined) {
             // clip-path
-            return character_node_size;
+            return char_node_size;
         }
         if (node.group == 0) {
             return movie_node_size;
         } else {
             // number after + is border width
-            return character_node_size + 1;
+            return char_node_size + 1;
         }
     }
 
@@ -120,7 +132,7 @@ function drawNodes() {
         .data(links)
         .enter().append("line")
         .attr("stroke-width", 1)
-        .attr("stroke", "rgba(50, 50, 50, 0.2)")
+        .attr("stroke", "rgba(255, 241, 191, 0.2)")
         .attr('id', function (linkObj) {
             var psource = processId(linkObj.source);
             var ptarget = processId(linkObj.target);
@@ -140,22 +152,33 @@ function drawNodes() {
         .attr('class', 'node')
         .append('clipPath').attr('id', "clip-circle")
         .append('circle')
-        .attr('r', character_node_size)
+        .attr('r', char_node_size)
 
 
     // create movie nodes
     nodeElements
         .filter(function (d) { return d.group == 0 })
         .attr('class', 'movie_node')
-        .append('circle')
-        .attr('r', movie_node_size)
-        .attr('id', function (d) {
-            return processId(d.id);
+        .selectAll('polygon')
+        .data([poly])
+        .enter().append("polygon")
+        .attr("points", function (d) {
+            return d.map(function (d) {
+                return [d.x - movie_node_width / 2, d.y - movie_node_height / 2].join(",");
+            }).join(" ");
         })
-        .attr('fill', 'white')
-        .attr('stroke', 'black')
-        .on("mouseover", handleMovieMouseOver)
-        .on("mouseout", reset)
+        .attr('fill', COLOR_BK)
+        .attr("stroke", "white")
+        .attr("stroke-width", 0.4);
+    // .append('circle')
+    // .attr('r', movie_node_size)
+    // .attr('id', function (d) {
+    //     return processId(d.id);
+    // })
+    // .attr('fill', 'black')
+    // .attr('stroke', 'white')
+    // .on("mouseover", handleMovieMouseOver)
+    // .on("mouseout", reset)
 
     // create char nodes
     nodeElements
@@ -165,27 +188,27 @@ function drawNodes() {
         })
         .attr('class', 'char_node')
         .append('circle')
-        .attr('r', character_node_size)
+        .attr('r', char_node_size)
         .attr('id', function (d) {
             return processId(d.id) + '_circle';
         })
         .attr('class', 'char_circle')
-        .attr('fill', 'white')
         .on("mouseover", handleCharMouseOver)
-        .on("mouseout", reset)
+        .on("click", handleCharMouseClick)
+        .on("mouseout", handleCharMouseOut)
 
 
     // add movie label, wrapped
     nodeElements
         .filter(function (d) { return d.group == 0 })
         .append('foreignObject')
-        .attr("x", -movie_node_size * 0.6)
-        .attr("y", -movie_node_size * 0.6)
-        .attr('width', movie_node_size * 1.2)
-        .attr('height', movie_node_size * 1.2)
-        .attr('font-size', '10px')
+        .attr("x", -movie_node_width * 0.45)
+        .attr("y", -movie_node_height * 0.4)
+        .attr('width', movie_node_width * 0.95)
+        .attr('height', movie_node_height)
+        .attr('font-size', '12px')
         .attr('font-weight', 'bold')
-        .attr('color', 'black')
+        .attr('color', 'white')
         .attr('class', 'movie_label')
         .append('xhtml:p')
         .text(function (node) {
@@ -264,7 +287,7 @@ function drawNodes() {
         d3.selectAll('.char_label')
             .style('opacity', 0)
         d3.selectAll('.char_circle')
-            .attr('r', character_node_size)
+            .attr('r', char_node_size)
 
         simulation.force("link").links(links)
         simulation
@@ -275,30 +298,32 @@ function drawNodes() {
     }
 
 
+
     nodeElements
         .filter(function (d) { return d.group == 1 })
         .append('image')
         .attr('href', function (d) { return d.photo; })
         .attr('class', 'profile_pic')
         .attr('id', function (d) { return processId(d.id); })
-        .attr('x', function (d) { return character_node_size * -1; })
-        .attr('y', function (d) { return character_node_size * -1; })
+        .attr('x', function (d) { return char_node_size * -1; })
+        .attr('y', function (d) { return char_node_size * -1; })
         .attr("clip-path", function (d, i) { return "url(#clip-circle)"; })
         .on("mouseover", handleCharMouseOver)
-        .on("mouseout", reset)
+        .on("click", handleCharMouseClick)
+    //.on("focusout", reset)
 
 
     // add character label, wrapped
     nodeElements
         .filter(function (d) { return d.group == 1 })
         .append('foreignObject')
-        .attr("x", - character_node_size)
-        .attr("y", character_node_size)
-        .attr('width', character_node_size * 2)
-        .attr('height', character_node_size)
+        .attr("x", - char_node_size)
+        .attr("y", char_node_size)
+        .attr('width', char_node_size * 2)
+        .attr('height', char_node_size)
         .attr('font-size', '10px')
         .attr('font-weight', 'bold')
-        .attr('color', 'red')
+        .attr('color', COLOR_RED)
         .append('xhtml:p')
         .text(function (node) {
             return node.id;
@@ -310,10 +335,64 @@ function drawNodes() {
         .attr('class', 'char_label')
         .style('opacity', 0)
 
+    function resetChar(charId) {
+        console.log('reset')
+        var selected_label = '#' + charId + '_label'
+        d3.select(selected_label)
+            .style('opacity', 0)
+
+        var selected_node = '#' + charId + '_circle'
+        //console.log(d3.select(selected_node))
+        d3.select(selected_node)
+            .attr('r', char_node_size)
+    }
 
     function handleCharMouseOver() {
-        //console.log(this)
+        if (hover_char && hover_char != active_char)
+            resetChar(hover_char);
+
+        // hahahah
+
         var selectedId = this.id;
+        hover_char = selectedId;
+
+        var selected_label = '#' + selectedId + '_label'
+        d3.select(selected_label)
+            .style('opacity', 1)
+
+        var selected_node = '#' + selectedId + '_circle'
+        console.log(d3.select(selected_node))
+        d3.select(selected_node)
+            .attr('r', char_node_size_enlarged)
+            .attr('fill', COLOR_RED)
+    }
+
+    function handleCharMouseOut() {
+        if (!hover_char)
+            return
+
+        var last_label = '#' + hover_char + '_label'
+        d3.select(last_label)
+            .style('opacity', 0)
+
+        var last_node = '#' + hover_char + '_circle'
+        console.log(d3.select(last_node))
+        d3.select(last_node)
+            .attr('r', char_node_size)
+
+        hover_char = null;
+    }
+
+
+
+
+    function handleCharMouseClick() {
+        var selectedId = this.id;
+
+        if (active_char)
+            resetChar(active_char);
+
+        active_char = selectedId;
 
         var connectedLinks = links.filter(function (link) {
             if (processId(link.source.id) == selectedId || processId(link.target.id) == selectedId)
@@ -368,12 +447,7 @@ function drawNodes() {
         console.log(d3.select(selected_node))
         d3.select(selected_node)
             .attr('r', char_node_size_enlarged)
-            .attr('fill', 'red')
-        // .attr('transform', function () {
-        //     //transform = d3.transform(d3.select(this).attr("transform"));
-        //     console.log(d3.select(this).attr("transform") + ",scale(1.5, 1.5)")
-        //     return "translate(", width / 2 - radius, "," + height / 2, "),scale(1.5)";
-        // })
+            .attr('fill', COLOR_RED)
 
         d3.selectAll('.link')
             .style('opacity', function (link) {
@@ -394,8 +468,6 @@ function drawNodes() {
 
 
     }
-
-
 
     function setUpMovies(node, counter) {
         var angle = (counter / (numOfMovie / 2)) * Math.PI;
